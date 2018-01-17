c8bb4e928cce389d911508ec69949d5c9600ac03
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-01-17 08:56:47 -0600

    Assets

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,20 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour {
+    public float jumpForce = 10f;
+    Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up);
+        }
+	}
+}
