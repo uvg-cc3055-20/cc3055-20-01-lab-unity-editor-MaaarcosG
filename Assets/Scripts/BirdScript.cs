2fc6f385a87b95ca44e8cad56133287c372282b0
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-01-17 09:25:08 -0600

    Script Salto

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BirdScript : MonoBehaviour {
-    public float jumpForce = 10f;
+    public float jumpForce;
     Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
-        rb = GetComponent<Rigidbody2D>();   
+        rb = GetComponent<Rigidbody2D>();
+        jumpForce = 500f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetButtonDown("Jump"))
         {
-            rb.AddForce(Vector2.up);
+            rb.AddForce(Vector2.up * jumpForce);
         }
 	}
 }
