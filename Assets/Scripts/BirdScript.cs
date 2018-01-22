d5b1c85c7c4921143ca6afd0d4fa679a43da2930
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-01-22 10:01:54 -0600

    Actualizada ultima version. Falta Extras

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -6,7 +6,7 @@ public class BirdScript : MonoBehaviour {
     public float jumpForce;
     public float forwardSpeed;
     public GameObject cam;
-    public bool dead = false;
+    public bool dead;
 
     Rigidbody2D rb;
 	// Use this for initialization
@@ -14,16 +14,24 @@ public class BirdScript : MonoBehaviour {
         rb = GetComponent<Rigidbody2D>();
         jumpForce = 300f;
         forwardSpeed = 2f;
+        dead = false;
+        rb.freezeRotation = true;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        cam.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
-        rb.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+
+    // Update is called once per frame
+    void Update() {
+        if (dead.Equals(false))
+        {
+            cam.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+            rb.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+        }
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = Vector2.zero;
             rb.AddForce(Vector2.up * jumpForce);
+        }
+        if (rb.position.x >= 35.47){
+            dead = true;
         }
 	}
     private void OnCollisionEnter2D(Collision2D collision)
